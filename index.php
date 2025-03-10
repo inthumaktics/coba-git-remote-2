fc3544274b988e3a6f17df831d9c1f14b0a2d72d
Inthumaktics <inthumakticss@gmail.com>
2025-03-10 10:33:36 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>COBA GIT REMOTE 2</title>
 </head>
 <body>
-    
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias iure facere aspernatur hic, earum veniam beatae dignissimos ipsa doloremque. Iste dolore explicabo nobis laborum sunt consequuntur, nemo doloribus tempora nisi!</p>
 </body>
 </html>
